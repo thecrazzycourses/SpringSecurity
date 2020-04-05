da363b1de02c3f3db2badfe2280e465811277ce5
rahul9582816659 <rahul9582816659@gmail.com>
2020-04-05 09:29:16 +0530

    in memory

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # SpringSecurity
 SpringSecurity
 
-1. feature/basic-auth : user and pass on console localhost:8080/api
+1. feature/basic-auth : user and pass on console, localhost:8080/api
 
-2. feature/in-memory
+2. feature/in-memory : user and password, localhost:8080/api
 
 3. feature/jdbc-auth
 
---
+++ src/main/java/com/crazzy/security/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.crazzy.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build()
+        );
+    }
+}
